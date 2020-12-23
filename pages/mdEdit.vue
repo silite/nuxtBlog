caba6aad8067d088215c0d45fd8f14c812c7bac2
silite <32181762+silite@users.noreply.github.com>
2020-12-23 15:00:15 +0800

    test jenkins error log

--- pages/mdEdit.vue
+++ pages/mdEdit.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script>
-import { defineComponent } from '@vue/composition-api'
+import { defineComponent } from '@vue/Composition-api'
 export default defineComponent({
   setup() {
     if (process.browser) {
